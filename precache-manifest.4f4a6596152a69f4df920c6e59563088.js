37bab8299654934f80d9cef56e4b7ab102566133
tal87 <talbsho@gmail.com>
2019-06-17 07:44:39 +0300

    Updates

--- precache-manifest.4f4a6596152a69f4df920c6e59563088.js
+++ precache-manifest.4f4a6596152a69f4df920c6e59563088.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "98cad162547597510b9e2d49624d65cc",
+    "revision": "c51619ee1100a61c1090bf9e642addc2",
     "url": "/tasks/index.html"
   },
   {
-    "revision": "60802468104ef546c775",
+    "revision": "957a521dfcce52335374",
     "url": "/tasks/static/css/main.c0034b7c.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tasks/static/js/2.345b61c0.chunk.js"
   },
   {
-    "revision": "60802468104ef546c775",
-    "url": "/tasks/static/js/main.5b237d86.chunk.js"
+    "revision": "957a521dfcce52335374",
+    "url": "/tasks/static/js/main.633cc692.chunk.js"
   },
   {
     "revision": "7d8089a231448516824e",
